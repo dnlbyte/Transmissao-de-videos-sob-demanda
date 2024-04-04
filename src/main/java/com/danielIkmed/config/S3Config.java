959ab4ac71b4193245f9ccf589663f491bb81f60
Daniel Silva De Lima <daniel_lma@hotmail.com>
2024-04-04 17:46:32 -0300

    Correção no package

--- src/main/java/com/danielIkmed/config/S3Config.java
+++ src/main/java/com/danielIkmed/config/S3Config.java
@@ -1,4 +1,5 @@
-package config;
+package com.danielIkmed.config;
+
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
