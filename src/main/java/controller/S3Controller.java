d1b2b578ccaef8da5ea2dd2275f39294d4915328
Daniel Silva De Lima <daniel_lma@hotmail.com>
2024-04-04 14:52:24 -0300

    Criação das classes de configuração e aplicação e a classe de controle ainda guardando o código

---
+++ src/main/java/controller/S3Controller.java
@@ -0,0 +1,20 @@
+package controller;
+
+import com.amazonaws.auth.policy.Resource;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping
+
+public class S3Controller {
+    
+}
