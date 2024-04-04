91623b60a7d1f8e9940f67026ad8686d9b5b85b2
Daniel Silva De Lima <daniel_lma@hotmail.com>
2024-04-04 15:49:57 -0300

    Criação dos métods /list e /upload, já atrelados a camada de serviço

--- src/main/java/controller/S3Controller.java
+++ src/main/java/controller/S3Controller.java
@@ -8,13 +8,31 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import service.S3Service;
 
 import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping
-
+@RequestMapping("api/v1")
 public class S3Controller {
-    
+    private final S3Service s3Service;
+
+    public S3Controller(S3Service s3Service){
+        this.s3Service = s3Service;
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<S3ObjectSummary>> listFiles(){
+        List<S3ObjectSummary> files = s3Service.lisFiles();
+
+        return ResponseEntity.ok(files);
+
+    }
+    @PostMapping(path = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public String uploadFile(@RequestParam("file") MultipartFile file)throws Exception{
+        s3Service.uploadFile(file.getOriginalFilename(), file);
+        return "file uploaded";
+    }
+
 }
